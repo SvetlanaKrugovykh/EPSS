bc33135b534ff95d54612a6ef708e0aa436caa1f
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-01-06 15:55:03 +0100

    preparing to next tests

--- src/server/controllers/epssController.js
+++ src/server/controllers/epssController.js
@@ -4,7 +4,7 @@ const addTag = process.env.PLATFORM !== 'freebsd' ? '( Test mode )' : ''
 
 module.exports.epssSign = async function (request, _reply) {
   const { FileName, KeyFiles } = request.body
-  const message = await epssService.signFile(FileName, KeyFiles, true)
+  const message = await epssService.signFile(FileName, KeyFiles)
 
   if (!message) {
     throw new HttpError[501]('Command execution failed')
